aed9343f96398948923340e8c1486f753bf4f930
dvdmakeev <dvdmakeev@gmail.com>
2022-06-15 09:39:27 +0300

    lexing draft

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 # e.g. 2 + 4 * (3 + 4) / -34
 # evaluate the expression
 
+from interpertation.interpreter import Interpreter
+
 def run():
     print("This is a simple interpreter. Write an expression to evaluete:")
     expr = input()
-    print(expr)
+    interpreter = Interpreter()
+
+    result = interpreter.interpret(expr)
+
+    print(result)
 
 
 if __name__ == '__main__':
--- parsing/parser.py
+++ parsing/parser.py
@@ -1,2 +1,11 @@
+from lexing.lexer import Lexer, Token, TokenType
+from nodes.node import Node
+
 class Parser:
-    pass
+    def __init__(self, lexer: Lexer) -> None:
+        self.lexer = lexer
+
+    def parse(self) -> Node:
+        token = self.lexer.get_next()
+        while token.type is not TokenType.EOF:
+            token = self.lexer.get_next()
